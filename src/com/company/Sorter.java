c29ebf62391eb44408a24ee07212617247e3302a
hadi-tghl <hadi@gmx.se>
2021-11-29 13:30:53 +0100

    Adding some comments to codes

--- src/com/company/Sorter.java
+++ src/com/company/Sorter.java
@@ -1,6 +1,7 @@
 package com.company;
 
 public class Sorter {
+    // printing methods to print out arrays
     public static void printArray(int[] args){
         for (int i : args) {
             System.out.println(i + " ");
@@ -8,12 +9,14 @@ public class Sorter {
         System.out.println();
     }
 
+    // method for swapings args from smallest to the largest
     public static void swap(int[] args, int left, int right){
         int temp = args[left];
         args[left] = args[right];
         args[right] = temp;
     }
 
+    // method for sorting with help of swap method
     public static void selctSort(int[] args){
         int min;
         for (int i = 0; i < args.length - 1; i++) {
