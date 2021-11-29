1417a4cc1743b98d818582037ee9d9a27b5dd4b6
Hadi <hadi@gmx.se>
2021-11-29 11:20:34 +0100

    Added some methods for sorting algorithms

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] argss = new int[args.length];
+        boolean isRunning = true;
+        for (int i = 0; i < argss.length; i++) {
+            if (isNumeric(args[i])){
+                argss[i] = Integer.parseInt(args[i]);
+
+            }
+            else {
+                isRunning = false;
+                System.out.println("You have entered wrong value! ");
+            }
+        }
+        if (isRunning ){
+            Sorter.selctSort(argss);
+            Sorter.printArray(argss);
+        }
+
+
+    }
+
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+
+
+    
+
+
+}
---
+++ src/com/company/Sorter.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Sorter {
+    public static void printArray(int[] args){
+        for (int i : args) {
+            System.out.println(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void swap(int[] args, int left, int right){
+        int temp = args[left];
+        args[left] = args[right];
+        args[right] = temp;
+    }
+
+    public static void selctSort(int[] args){
+        int min;
+        for (int i = 0; i < args.length - 1; i++) {
+            min = i;
+            for (int j = i + 1; j < args.length ; j++) {
+                if (args[j] < args[min]){
+                    min = j;
+                }
+            }
+            if (min != i){
+                swap(args, i, min);
+            }
+        }
+    }
+
+}
