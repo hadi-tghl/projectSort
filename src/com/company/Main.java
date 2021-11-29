1b789b24fc2a105b04f0fab84c9bfa1b2a360734
Hadi <hadi@gmx.se>
2021-11-29 11:49:46 +0100

    Adding some comments to code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,11 +8,10 @@ public class Main {
         for (int i = 0; i < argss.length; i++) {
             if (isNumeric(args[i])){
                 argss[i] = Integer.parseInt(args[i]);
-
             }
             else {
                 isRunning = false;
-                System.out.println("You have entered wrong value! ");
+                System.out.println("You have entered wrong value! Please enter Numeric value ");
             }
         }
         if (isRunning ){
@@ -23,6 +22,7 @@ public class Main {
 
     }
 
+    // method to verify user input is a number not a Srtring
     public static boolean isNumeric(String strNum) {
         if (strNum == null) {
             return false;
