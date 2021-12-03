1dfa52ef51064c620a5ca88b9ad003253e1b7c39
hadi-tghl <hadi@gmx.se>
2021-12-03 09:13:10 +0100

    Changing swap methods comments

--- src/com/company/Sorter.java
+++ src/com/company/Sorter.java
@@ -9,14 +9,14 @@ public class Sorter {
         System.out.println();
     }
 
-    // Swap method for integrating Integer and swapings from right to left depending on a input value.
+    // Swap method for integrating Integer and swapings from right to left.
     public static void swap(int[] args, int left, int right){
         int temp = args[left];
         args[left] = args[right];
         args[right] = temp;
     }
 
-    // method for sorting a number from smallest to the bigger with help of swap method.
+    // method for sorting a number from smallest to highest value with help of swap method.
     public static void selctSort(int[] args){
         int min;
         for (int i = 0; i < args.length - 1; i++) {
